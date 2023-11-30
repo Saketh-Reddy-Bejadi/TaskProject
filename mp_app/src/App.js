7d1c44c0ae95ad19b9a435f800c231a86e3098ab
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-01 01:42:10 +0530

    used some conditons

--- mp_app/src/App.js
+++ mp_app/src/App.js
@@ -9,15 +9,19 @@ export const App = () => {
     {id:6042,name:"Build or clone a E-Commerence Website",completed:false},
   ])
 
+
   function handleDelete(id) {
     setTasks(tasks.filter((task)=> id!==task.id))
+    
   }
+  const [show,setShow]=useState(true);
 
   return (
     <div className='App'>
       <h1 className='Heading' >Tasks List</h1>
-      <ul className='TaskItems' >
-        {tasks.map((task)=>(
+      <ul className={tasks.length!==0?'TaskItems':''}>
+        <input type='button' onClick={()=>setShow(!(show))} className={tasks.length!==0?'toggle':'disnone'} value={(show===true)?"Hide":"Show"} ></input>
+        {show && tasks.map((task)=>(
           <li key={task.id} className={(task.completed===true)?"completed":"notcompleted"} >
             <span>{task.id} - {task.name}</span>
             <button onClick={()=>handleDelete(task.id)} className='Deletebtn' >Delete</button>
@@ -27,4 +31,5 @@ export const App = () => {
       </ul>
     </div>
   )
+  
 }
