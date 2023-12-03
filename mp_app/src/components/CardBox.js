0ccb2e98838540549caa58ee46ab7675ecc89f13
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-04 02:04:19 +0530

    done with module-4

--- mp_app/src/components/CardBox.js
+++ mp_app/src/components/CardBox.js
@@ -7,9 +7,11 @@ export const CardBox = ({children,imp}) => {
     const [show,setShow]=useState(true);
 
   return (
-    <div  className={`box ${imp}`} >
-        {children} 
-        <button onClick={()=>setShow(!(show))}>Hide</button>
+    <div className={show?"":"disnone"} >
+        <div  className={`box ${imp}`} >
+          {children} 
+          <button className="Hidebtn" onClick={()=>setShow(!(show))}>Hide</button>
+        </div>
     </div>
   )
 }
