e93a9b3436d9d4b0355bd7f1a7919382c25efff9
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-06 23:08:03 +0530

    modified

--- mp_app/src/components/TaskList.js
+++ mp_app/src/components/TaskList.js
@@ -1,6 +1,5 @@
 import {  useState } from "react";
 import { TaskCard } from "./TaskCard";
-import { CardBox } from "./CardBox";
 import styles from  "./TaskList.module.css"
 
 export const TaskList = () => {
@@ -26,18 +25,6 @@ export const TaskList = () => {
             <TaskCard key={task.id+1} task={task} handleDelete={handleDelete}  />
             ))}
         </ul>
-        <CardBox imp="success" >
-            <h1>Success!</h1>
-            <p> Eaque ducimus amet temporibus, aspernatur cupiditate quas aut iste autem atque nemo, qui nam placeat officiis eius, corrupti dolores? Ipsum aperiam officia maiores beatae culpa.</p>
-        </CardBox>
-        <CardBox imp="warning" >
-            <h1>Warning!</h1>
-            <p>Lillum placeat vitae cumque in corrupti! Delectus, praesentium numquam. Harum sunt earum asperiores nostrum. Natus ipsa sint ea corrupti, explicabo soluta culpa quisquam facilis.</p>
-        </CardBox>
-        <CardBox imp="alert" >
-            <h1>Alert!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat mollitia placeat ipsa libero obcaecati, doloremque omnis commodi voluptas incidunt veritatis?</p>
-        </CardBox>
 
     </div>
   )
