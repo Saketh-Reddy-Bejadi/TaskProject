81a1bee1d6879e942acf3eb71afa54edfae08e53
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2023-12-07 23:43:39 +0530

    done with footer

--- mp_app/src/components/Footer.js
+++ mp_app/src/components/Footer.js
@@ -1,6 +1,6 @@
 
 export const Footer = () => {
   return (
-    <div className="Footer" >@TaskVista</div>
+    <div className="Footer" >@TaskVista-2023</div>
   )
 }
--- mp_app/src/components/Header.js
+++ mp_app/src/components/Header.js
@@ -5,8 +5,11 @@ export const Header = () => {
   return (
     <div>
         <nav>
+          <div className="navSec1" >
             <img className="imglogo" src={logo}  alt="" />
             <p className='Heading'>TaskVista</p>
+          </div>
+          <div><p className="home">Home</p></div>
         </nav>
     </div>
   )
